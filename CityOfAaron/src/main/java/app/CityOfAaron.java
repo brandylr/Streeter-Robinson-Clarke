e8379d80f7b7b18505cdd5edcb8daaabcc0503a5
brandylr <35279783+brandylr@users.noreply.github.com>
2018-10-13 19:27:21 -0500

    Updating files for individual assignment.

--- CityOfAaron/src/main/java/app/CityOfAaron.java
+++ CityOfAaron/src/main/java/app/CityOfAaron.java
@@ -7,6 +7,8 @@ import model.Game;
 import model.Player;
 import model.Map;
 import model.Storehouse;
+import model.InventoryItem;
+import model.Provision;
 
 
 
@@ -25,9 +27,21 @@ public class CityOfAaron {
         theGame.setThePlayer(new Player());
         theGame.setTheMap(new Map());
         theGame.setTheStorehouse(new Storehouse());
+        theGame.setCurrentPopulation(105);
+        theGame.setAcresOwned(2000);
+        theGame.setWheatInStorage(10000);
         
-
-        System.out.println("It works!");
+        System.out.println(theGame.toString());
+        
+        InventoryItem theInventoryItem = new InventoryItem();
+        
+        System.out.println(theInventoryItem.toString());
+        
+        Provision theProvision = new Provision();
+        
+        System.out.println(theProvision.toString());
+        
+        
 
     }
 
